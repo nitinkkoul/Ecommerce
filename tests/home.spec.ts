ddef19a7e0223db1c444400117fa6c2d90d453eb
nitinkoul <nitinkkoul@gmail.com>
2026-09-11 18:11:50 +0530

    changing node version

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -37,14 +37,12 @@ test('filters', async ({ page }) => {
     }
 });
 
-test.only('Selectcheckbo', async ({ page }) => {
+test('Selectcheckbo', async ({ page }) => {
     await homePageobj.selectchechbox('fashion');
     await homePageobj.selectchechbox('household');
     await expect.soft(homePageobj.maximumnumberofProductShowing).toContainText('Showing 0 results');
     await homePageobj.selectchechbox('electronics');
     await expect.soft(homePageobj.maximumnumberofProductShowing).toContainText('Showing 3 results');
-
-
 });
 
 
