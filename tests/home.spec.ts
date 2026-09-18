a93d1123b99fba01e2f58a241e51067014521c2a
nitinkoul <nitinkkoul@gmail.com>
2026-09-19 01:23:20 +0530

    checking alone

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -7,43 +7,43 @@ const searchdata = JSON.parse(fs.readFileSync(pathfordata, 'utf-8'));
 
 let homePageobj: homepage;
 
-test.beforeEach(async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await page.goto(process.env.BASE_URL!);
-    await loginPage.login(process.env.LOGIN_USERNAME!, process.env.LOGIN_PASSWORD!)
-    homePageobj = new homepage(page);
+// test.beforeEach(async ({ page }) => {
+//     const loginPage = new LoginPage(page);
+//     await page.goto(process.env.BASE_URL!);
+//     await loginPage.login(process.env.LOGIN_USERNAME!, process.env.LOGIN_PASSWORD!)
+//     homePageobj = new homepage(page);
 
-});
-test('home page test elements or texts', async ({ page }) => {
-    await expect.soft(homePageobj.maximumnumberofProductShowing).toBeVisible();
-    await expect.soft(homePageobj.maximumtext).toBeVisible();
-    await expect.soft(homePageobj.maximumnumberofProductShowing).toContainText('Showing 3 results');
-    await expect.soft(homePageobj.maximumtext).toContainText(' User can only see maximum 9 products on a page');
-    const numberOfProducts = await homePageobj.getNumberOfProductsOnPage();
-    expect.soft(numberOfProducts).toBe(3);
-    const totanumberOfProducts = await homePageobj.gettextofProductsOnPage();
-    console.log(totanumberOfProducts);
-    expect.soft(totanumberOfProducts).toEqual(['ADIDAS ORIGINAL', 'ZARA COAT 3', 'IPHONE 13 PRO']);
+// });
+// test('home page test elements or texts', async ({ page }) => {
+//     await expect.soft(homePageobj.maximumnumberofProductShowing).toBeVisible();
+//     await expect.soft(homePageobj.maximumtext).toBeVisible();
+//     await expect.soft(homePageobj.maximumnumberofProductShowing).toContainText('Showing 3 results');
+//     await expect.soft(homePageobj.maximumtext).toContainText(' User can only see maximum 9 products on a page');
+//     const numberOfProducts = await homePageobj.getNumberOfProductsOnPage();
+//     expect.soft(numberOfProducts).toBe(3);
+//     const totanumberOfProducts = await homePageobj.gettextofProductsOnPage();
+//     console.log(totanumberOfProducts);
+//     expect.soft(totanumberOfProducts).toEqual(['ADIDAS ORIGINAL', 'ZARA COAT 3', 'IPHONE 13 PRO']);
 
 
-});
+// });
 
-test('filters', async ({ page }) => {
-    for (const filter of searchdata.searchitems) {
-        await homePageobj.enterTermInSearchFilter(filter.name);
-        await page.keyboard.press('Enter');
-        await expect.soft(homePageobj.maximumnumberofProductShowing).toContainText('Showing 1 results');
+// test('filters', async ({ page }) => {
+//     for (const filter of searchdata.searchitems) {
+//         await homePageobj.enterTermInSearchFilter(filter.name);
+//         await page.keyboard.press('Enter');
+//         await expect.soft(homePageobj.maximumnumberofProductShowing).toContainText('Showing 1 results');
 
-    }
-});
+//     }
+// });
 
-test('Selectcheckbo', async ({ page }) => {
-    await homePageobj.selectchechbox('fashion');
-    await homePageobj.selectchechbox('household');
-    await expect.soft(homePageobj.maximumnumberofProductShowing).toContainText('Showing 0 results');
-    await homePageobj.selectchechbox('electronics');
-    await expect.soft(homePageobj.maximumnumberofProductShowing).toContainText('Showing 3 results');
-});
+// test('Selectcheckbo', async ({ page }) => {
+//     await homePageobj.selectchechbox('fashion');
+//     await homePageobj.selectchechbox('household');
+//     await expect.soft(homePageobj.maximumnumberofProductShowing).toContainText('Showing 0 results');
+//     await homePageobj.selectchechbox('electronics');
+//     await expect.soft(homePageobj.maximumnumberofProductShowing).toContainText('Showing 3 results');
+// });
 // test.only('addproduct', async ({ page }) => {
 //     await homePageobj.addProductandDelete('ZARA COAT 3');
 
